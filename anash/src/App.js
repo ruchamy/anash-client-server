152c2f272b1605199ca0db0d64fbc40eb0aecaee
ruchamy <r0527187861@gmail.com>
2025-07-22 19:48:20 +0300

    Refactor navigation links and clean up unused code in App component

--- anash/src/App.js
+++ anash/src/App.js
@@ -16,20 +16,14 @@ function App() {
   const logedInUser = useSelector(state => state.users.loggedInUser);
   const isAdmin = useSelector(state => state.users.isAdmin);
 
-  // useEffect(() => {
-  //   console.log('logedInUser:', logedInUser);
-  // }
-  //   , [logedInUser]);
   return (
     <>
       <header className={`app-header ${logedInUser ? '' : 'logged-out'}`}>
         {logedInUser && <>
           < nav className={`app-nav ${logedInUser ? '' : 'logged-out'}`}>
-            <Link className='app-Link active' to='./chat'>עדכוני אנ"ש</Link>
             <Link className='app-Link active' to='/temp'>תיווך דירות</Link>
             <Link className='app-Link active' to='/temp'>חדש בקהילה</Link>
             <Link className='app-Link active' to='/contacts'>רשימת אנ"ש</Link>
-            <Link className='app-Link active' to='/temp'>תמיכה הדדית</Link>
             {isAdmin ?
               <Link className='app-Link active' to='/admin'>ניהול</Link>
               : <Link className='app-Link active' to='/contact'>צור קשר</Link>}
@@ -41,13 +35,12 @@ function App() {
           className={`logo-image` + (logedInUser ? ' logged-in' : ' logged-out')}
         />
       </header>
-      <ShowAds/>
+      {logedInUser && <ShowAds />}
       <div className={`container ${logedInUser ? '' : 'logged-out'}`}>
         {/* <ShowAds/> */}
         <Routes>
           <Route path="/" element={logedInUser ? <div></div> : <Home />} />
           <Route path="/temp" element={<UnderConstruction />} />
-          <Route path="/chat" element={<iframe src="/chat.html" width="100%" height="600px" />} />
           <Route path="/contacts" element={<UserList />} />
           <Route path="/contact" element={<ContactForm />} />
           <Route path="/admin" element={<AdminPanel />} />
@@ -55,6 +48,7 @@ function App() {
           <Route path="/registration" element={<RegistrationForm />} />
         </Routes>
       </div>
+      <ShowAds />
       <footer className={`app-footer ${logedInUser ? '' : 'logged-out'}`}>
         <p>© כל הזכויות שמורות, פותח על ידי <bdi><a href='mailto:r0527187861@gmail.com' target="_blank">r0527187861@gmail.com</a></bdi> 2025.</p>
 
